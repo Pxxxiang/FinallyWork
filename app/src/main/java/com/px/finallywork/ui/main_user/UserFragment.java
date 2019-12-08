13c4063ae6803f2e27935bbb456f7f11f1504ef1
Pxxxiang <805700086@qq.com>
2019-12-09 00:26:58 +0800

    个人页面布局scrollview的bug修复

--- app/src/main/java/com/px/finallywork/ui/main_user/UserFragment.java
+++ app/src/main/java/com/px/finallywork/ui/main_user/UserFragment.java
@@ -1,8 +1,10 @@
 package com.px.finallywork.ui.main_user;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -20,48 +22,44 @@ import com.px.finallywork.R;
 
 public class UserFragment extends Fragment {
 
-    private ImageButton ibUserIconAvator;
     private ImageButton ibUserMessage;
+    private ScrollView scrollView;
+    private ImageButton ibUserIconAvator;
+    private TextView tvUsercenter;
 
+    @SuppressLint("Range")
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View  view= inflater.inflate(R.layout.fragment_main_user, container, false);
 
+
         findView(view);
-
-        ibUserIconAvator.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+        tvUsercenter.setAlpha(0);
         ibUserMessage.setOnClickListener(view2 -> {
             Intent intent = new Intent(getContext(), MessageCenterActivity.class);
             startActivity(intent);
+        });
+
+        scrollView.setOnTouchListener((v, event) -> {
+            int[] location = new int[2];
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    break;
+                case MotionEvent.ACTION_MOVE://下滑是正，上滑是负
+                    ibUserIconAvator.getLocationOnScreen(location);//初始状态为125,即最大值是125，全部显示不透明是（40？）
+                    float i = (location[1] - 40) / 85f;
+                    tvUsercenter.setAlpha(1 - i);
+                    break;
+            }
+            return false;
         });
         return view;
     }
 
     private void findView(View view) {
-        ibUserIconAvator =  view.findViewById(R.id.ib_user_icon_avator);
-        TextView tvUsername = view.findViewById(R.id.tv_username);
-        TextView tvAllOrder = view.findViewById(R.id.tv_all_order);
-        TextView tvUserPay = view.findViewById(R.id.tv_user_pay);
-        TextView tvUserReceive = view.findViewById(R.id.tv_user_receive);
-        TextView tvUserFinish = view.findViewById(R.id.tv_user_finish);
-        TextView tvUserDrawback = view.findViewById(R.id.tv_user_drawback);
-        TextView tvUserLocation = view.findViewById(R.id.tv_user_location);
-        TextView tvUserCollect = view.findViewById(R.id.tv_user_collect);
-        TextView tvUserCoupon = view.findViewById(R.id.tv_user_coupon);
-        TextView tvUserScore = view.findViewById(R.id.tv_user_score);
-        TextView tvUserPrize = view.findViewById(R.id.tv_user_prize);
-        TextView tvUserTicket = view.findViewById(R.id.tv_user_ticket);
-        TextView tvUserInvitation = view.findViewById(R.id.tv_user_invitation);
-        TextView tvUserCallcenter = view.findViewById(R.id.tv_user_callcenter);
-        TextView tvUserFeedback = view.findViewById(R.id.tv_user_feedback);
-        TextView tvUsercenter = view.findViewById(R.id.tv_usercenter);
-        ImageButton ibUserSetting = view.findViewById(R.id.ib_user_setting);
+        ibUserIconAvator = view.findViewById(R.id.ib_user_icon_avator);
+        tvUsercenter = view.findViewById(R.id.tv_usercenter);
         ibUserMessage = view.findViewById(R.id.ib_user_message);
-        ScrollView scrollView = view.findViewById(R.id.scrollview);
+        scrollView = view.findViewById(R.id.scrollview);
     }
 }
